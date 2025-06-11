5ba104131ef45fc88c610ac9c59dc355cab68845
Andrew Brown <ab36245@gmail.com>
2025-06-11 18:09:52 +1000

    codec changes

---
+++ decoder.go
@@ -0,0 +1,112 @@
+package msgs
+
+import (
+	"time"
+
+	"github.com/ab36245/go-codec"
+	"github.com/ab36245/go-msgpack"
+)
+
+func Decoder(b []byte) *decoder {
+	mp := msgpack.NewDecoder(b)
+	id := int(mp.GetInt())
+	return &decoder{
+		objectDecoder: *newObjectDecoder(mp),
+		id:            id,
+	}
+}
+
+type decoder struct {
+	objectDecoder
+	id int
+}
+
+func (d *decoder) Id() int {
+	return d.id
+}
+
+func newObjectDecoder(mp *msgpack.Decoder) *objectDecoder {
+	return &objectDecoder{
+		mp: mp,
+	}
+}
+
+type objectDecoder struct {
+	mp *msgpack.Decoder
+}
+
+func (s *objectDecoder) GetArray(name string) (codec.ArrayDecoder, error) {
+	return decodeArray(s.mp)
+}
+
+func (s *objectDecoder) GetDate(name string) (time.Time, error) {
+	return decodeDate(s.mp)
+}
+
+func (s *objectDecoder) GetInt(name string) (int, error) {
+	return decodeInt(s.mp)
+}
+
+func (s *objectDecoder) GetObject(name string) (codec.ObjectDecoder, error) {
+	return decodeObject(s.mp)
+}
+
+func (s *objectDecoder) GetString(name string) (string, error) {
+	return decodeString(s.mp)
+}
+
+func newArrayDecoder(mp *msgpack.Decoder) *arrayDecoder {
+	return &arrayDecoder{
+		mp:     mp,
+		length: mp.GetArrayLength(),
+	}
+}
+
+type arrayDecoder struct {
+	mp     *msgpack.Decoder
+	length int
+}
+
+func (s *arrayDecoder) GetArray() (codec.ArrayDecoder, error) {
+	return decodeArray(s.mp)
+}
+
+func (s *arrayDecoder) GetDate() (time.Time, error) {
+	return decodeDate(s.mp)
+}
+
+func (s *arrayDecoder) GetInt() (int, error) {
+	return decodeInt(s.mp)
+}
+
+func (s *arrayDecoder) GetObject() (codec.ObjectDecoder, error) {
+	return decodeObject(s.mp)
+}
+
+func (s *arrayDecoder) GetString() (string, error) {
+	return decodeString(s.mp)
+}
+
+func (s *arrayDecoder) Length() int {
+	return s.length
+}
+
+func decodeDate(mp *msgpack.Decoder) (time.Time, error) {
+	return mp.GetTime(), nil
+}
+
+func decodeArray(mp *msgpack.Decoder) (*arrayDecoder, error) {
+	return newArrayDecoder(mp), nil
+}
+
+func decodeInt(mp *msgpack.Decoder) (int, error) {
+	return int(mp.GetInt()), nil
+}
+
+func decodeObject(mp *msgpack.Decoder) (*objectDecoder, error) {
+	return newObjectDecoder(mp), nil
+}
+
+func decodeString(mp *msgpack.Decoder) (string, error) {
+	return mp.GetString(), nil
+}
---
+++ encoder.go
@@ -0,0 +1,108 @@
+package msgs
+
+import (
+	"time"
+
+	"github.com/ab36245/go-codec"
+	"github.com/ab36245/go-msgpack"
+)
+
+func Encoder() *encoder {
+	mp := msgpack.NewEncoder()
+	return &encoder{
+		objectEncoder: *newObjectEncoder(mp),
+	}
+}
+
+type encoder struct {
+	objectEncoder
+}
+
+func (e *encoder) Bytes() []byte {
+	return e.mp.Bytes()
+}
+
+func (e *encoder) Id(id int) {
+	e.mp.PutInt(int64(id))
+}
+
+func newObjectEncoder(mp *msgpack.Encoder) *objectEncoder {
+	return &objectEncoder{
+		mp: mp,
+	}
+}
+
+type objectEncoder struct {
+	mp *msgpack.Encoder
+}
+
+func (e *objectEncoder) PutArray(name string, length int, handler func(codec.ArrayEncoder)) {
+	encodeArray(e.mp, length, handler)
+}
+
+func (e *objectEncoder) PutDate(name string, value time.Time) {
+	encodeDate(e.mp, value)
+}
+
+func (e *objectEncoder) PutInt(name string, value int) {
+	encodeInt(e.mp, value)
+}
+
+func (e *objectEncoder) PutObject(name string, handler func(codec.ObjectEncoder)) {
+	encodeObject(e.mp, handler)
+}
+
+func (e *objectEncoder) PutString(name string, value string) {
+	encodeString(e.mp, value)
+}
+
+func newArrayEncoder(mp *msgpack.Encoder, length int) *arrayEncoder {
+	mp.PutArrayLength(length)
+	return &arrayEncoder{
+		mp: mp,
+	}
+}
+
+type arrayEncoder struct {
+	mp *msgpack.Encoder
+}
+
+func (e *arrayEncoder) PutArray(length int, handler func(codec.ArrayEncoder)) {
+	encodeArray(e.mp, length, handler)
+}
+
+func (e *arrayEncoder) PutDate(value time.Time) {
+	encodeDate(e.mp, value)
+}
+
+func (e *arrayEncoder) PutInt(value int) {
+	encodeInt(e.mp, value)
+}
+
+func (e *arrayEncoder) PutObject(handler func(codec.ObjectEncoder)) {
+	encodeObject(e.mp, handler)
+}
+
+func (e *arrayEncoder) PutString(value string) {
+	encodeString(e.mp, value)
+}
+
+func encodeArray(mp *msgpack.Encoder, length int, handler func(codec.ArrayEncoder)) {
+	handler(newArrayEncoder(mp, length))
+}
+
+func encodeDate(mp *msgpack.Encoder, value time.Time) {
+	mp.PutTime(value)
+}
+
+func encodeInt(mp *msgpack.Encoder, value int) {
+	mp.PutInt(int64(value))
+}
+
+func encodeObject(mp *msgpack.Encoder, handler func(codec.ObjectEncoder)) {
+	handler(newObjectEncoder(mp))
+}
+
+func encodeString(mp *msgpack.Encoder, value string) {
+	mp.PutString(value)
+}
